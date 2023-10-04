0a2d07535fd7f47ca2f888ee51c2013162f8f56a
salkingjust <144795808+salkingjust@users.noreply.github.com>
2023-10-04 10:36:39 +0100

    please merge
    please merge

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 class Person:
-    def __init__(self):
+    def _init_(self):
         self.first_name = ""
         self.last_name = ""
         self.age = 0
 
-    def user_input(self):
+    def get_user_input(self):
         self.first_name = input("Enter your first name: ")
         self.last_name = input("Enter your last name: ")
         self.age = int(input("Enter your age: "))
 
-    def display(self):
-        print(f"firstname = {self.first_name}")
-        print(f"lastname = {self.last_name}")
-        print(f"age = {self.age}")
+
+
+    def display_info(self):
+        print(f"First Name: {self.first_name}")
+        print(f"Last Name: {self.last_name}")
+        print(f"Age: {self.age}")
+
 def main():
-    user = Person
-    user.user_input(self=)
-    user.display(self=)
+    user = Person()
+    user.get_user_input()
+    user.display_info()
 
 main()
-
-
-
-
